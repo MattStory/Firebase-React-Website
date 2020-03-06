c1512b1b29111e0173dc002f8b22e97ed45a6dd9
Jeremy Chen <jeremy.chen@outlook.com>
2020-03-05 21:05:28 -0500

    Create a new doc for user when creating first transaction

--- src/store/actions/transactionActions.js
+++ src/store/actions/transactionActions.js
@@ -1,32 +1,36 @@
 export const createTransaction = (transaction) => {
     return (dispatch, getState, {getFirebase, getFirestore} ) => {
         const firestore = getFirestore();
-        const profile = getState().firebase.profile;
         const userId = getState().firebase.auth.uid;
-
+        const profile = getState().firebase.profile;
         //check if user's collection has been created
-        let docRef = firestore.collection('transactions').doc(userId);
+        let docRef = firestore.collection("transactions").doc(userId);
 
-        docRef.get().then(function (doc) {
-            if (!doc.exists){
-                docRef = firestore.collection('transactions').add({
-                    userId: userId,
+        docRef.get().then(function(doc) {
+            if (!doc.exists) {
+                docRef.set({
                     owner: profile.firstName + ' ' + profile.lastName,
-                    createdAt: new Date()
+                    uid: userId
                 })
             }
-        }).catch(function (err) {
-            console.log("Error getting document:", err);
+
+            docRef = docRef.collection('userTransactions');
+
+            docRef.add({
+                ...transaction,
+                createdAt: new Date()
+            }).then(() => {
+                dispatch({ type: 'CREATE_FUND', transaction });
+                alert("Transaction Created");
+            }).catch((err) => {
+                dispatch({ type: 'CREATE_FUND_ERR'}, err);
+                alert("Create transaction failed.\n" + err.message);
+            })
+
+        }).catch(function(error) {
+            console.log("Error getting document:", error);
         });
 
-        docRef.set({
-            ...transaction
-        }).then(() => {
-            dispatch({ type: 'CREATE_FUND', transaction });
-            alert("Transaction Created");
-        }).catch((err) => {
-            dispatch({ type: 'CREATE_FUND_ERR'}, err);
-            alert("Create transaction failed.\n" + err.message);
-        })
+
     }
 };
