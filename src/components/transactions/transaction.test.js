376e0dceff7c124b8bf2e3c893999effd7fe1262
Jethro Zhou <jethro.zhou@LIVE.com>
2020-04-23 03:36:46 -0400

    new

--- src/components/transactions/transaction.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Transactions from './Transactions';
-import renderer from 'react-test-renderer';
-
-test('matches snapshot', () => {
-  const tree = renderer.create(<Transactions />).toJSON();
-
-  expect(tree).toMatchSnapshot();
-})
